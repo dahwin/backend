7d16532460684e6ec20b6806b4d9b1a6b5fa018e
dahwin <116961692+dahwin@users.noreply.github.com>
2023-12-19 23:25:01 +0600

    database

---
+++ database.py
@@ -0,0 +1,32 @@
+from model import Todo
+
+import motor.motor_asyncio
+
+client = motor.motor_asyncio.AsyncIOMotorClient('mongodb://localhost:27017')
+
+database = client.todolist
+collection = database.todo
+
+async def fetch_one_todo(title):
+        documents = await collection.find_one({'title':title})
+
+async def fetch_all_todos():
+        todos = []
+
+        cursor = collection.find({})
+        async for document in cursor:
+                todos.append(Todo(**document))
+
+        return todos
+async def create_todo(todo):
+        documents =todo
+        result = await collection.insert_one(documents)
+        return documents
+async def update_todo(title,desc):
+        await collection.update_one({'title':title},{'$set':{'description':desc}})
+        document = await collection.find_one({'title':title})
+        return document
+async def remove_todo(title):
+    await collection.delete_one({"title": title})
+    return True
+
---
+++ main.py
@@ -0,0 +1,55 @@
+from fastapi import FastAPI,HTTPException
+from fastapi.middleware.cors import CORSMiddleware
+app = FastAPI()
+from database import *
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI()
+
+origins = ['http://localhost:3000']
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+# Your routes go here
+
+@app.get('/')
+def read_root():
+    return {'dahyun':'darwin'}
+@app.get('/api/todo')
+async def get_todo():
+    return_dahwin = await fetch_all_todos()
+    return return_dahwin
+
+@app.get('/api/todo{title}',response_model=Todo)
+async def get_todo_by_id(title):
+    dahwin_res = await fetch_one_todo(title)
+    if dahwin_res:
+        return dahwin_res
+    raise HTTPException(404,f'{title} is not found')
+
+
+@app.post('/api/todo',response_model=Todo)
+async def post_todo(todo:Todo):
+    dahwin_res = await create_todo(todo.dict())
+    if dahwin_res:
+        return dahwin_res
+    raise HTTPException('404 something went wrong')
+
+@app.put('/api/todo{title}',response_model=Todo)
+async def put_todo(title,desc):
+    dahwin_res = await update_todo(title,desc)
+    if dahwin_res:
+        return dahwin_res
+    raise HTTPException(404,f'{title} is not found')
+@app.delete("/api/todo/{title}")
+async def delete_todo(title):
+    response = await remove_todo(title)
+    if response:
+        return "Successfully deleted todo"
+    raise HTTPException(404, f"There is no todo with the title {title}")
