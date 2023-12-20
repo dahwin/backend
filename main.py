c98ef1527b01b51ec5b2737e4ac0a2c787c62a75
dahwin <116961692+dahwin@users.noreply.github.com>
2023-12-20 07:53:09 +0600

    d

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ from fastapi.middleware.cors import CORSMiddleware
 app = FastAPI()
 
 # Allow requests from your React app running on localhost:3000
-origins = ['https://www.queendahyun.com/']
+origins = ['http://localhost:3000']
 app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,
